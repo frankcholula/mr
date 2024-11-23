749be565d597d8d744a28cb9bd29d4d75c4a6708
frankcholula <lu.phrank@gmail.com>
2024-11-23 14:06:00 +0000

    test simulation.

--- assignment1/trossen/simulation.py
+++ assignment1/trossen/simulation.py
@@ -37,6 +37,28 @@ links = [
 ]
 
 robot = DHRobot(links, name='PincherX 100')
-print(robot)
-q = [0, 0, 0, 0]
-robot.plot(q, backend='pyplot', block=True)
+
+# Define a sequence of joint angles to move through (in radians)
+q1 = [0, 0, 0, 0]
+q2 = [np.pi/4, np.pi/4, 0, 0]
+q3 = [np.pi/4, np.pi/4, np.pi/4, 0]
+q4 = [np.pi/4, np.pi/4, np.pi/4, -np.pi/4, 0]
+q5 = [0, 0, 0, 0]
+
+# Create trajectory through these points
+steps = 10
+trajectory = []
+
+for i in range(len([q1,q2,q3,q4,q5])-1):
+    start = [q1,q2,q3,q4,q5][i]
+    end = [q1,q2,q3,q4,q5][i+1]
+    for step in range(steps):
+        t = step/steps  # Interpolation parameter
+        q = []
+        for j in range(4):  # For each joint
+            angle = start[j] + t*(end[j] - start[j])
+            q.append(angle)
+        trajectory.append(q)
+
+for q in trajectory:
+    robot.plot(q, block=False, limits=[-0.2, 0.2, -0.2, 0.2, 0, 0.4])
